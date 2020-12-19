48d9765c482f29fa753abd2fbee1745ed7e39378
uwrf-csis <13894601+uwrf-csis@users.noreply.github.com>
2020-12-19 13:54:43 -0600

    Create __init__.py

---
+++ script/__init__.py
@@ -0,0 +1,12 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """typescript exists"""
+    check50.exists("typescript")
+
+@check50.check(exists)
+def contains():
+    """commands look good"""
+    check50.run("grep -w 'ls' typescript").stdout("1").exit()
