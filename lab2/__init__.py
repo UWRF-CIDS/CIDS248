734dbea9a5d33b4ca071f4959e162bd54a0e95b1
Jacob <jacob.hendricks@uwrf.edu>
2020-12-21 13:13:16 -0600

    fixed check for lab2

--- lab2/__init__.py
+++ lab2/__init__.py
@@ -10,7 +10,7 @@ def exists():
 def contains_cp():
     """command "cp" is present"""
     output = check50.run("grep -c -w 'cp' typescript").stdout()   
-    if output == "0":
+    if output == "0\n":
         help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
         raise check50.Failure(help)
 
@@ -18,7 +18,7 @@ def contains_cp():
 def contains_mv():
     """command "mv" is present"""
     output = check50.run("grep -c -w 'mv' typescript").stdout()   
-    if output == "0":
+    if output == "0\n":
         help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
         raise check50.Failure(help)
   
@@ -26,7 +26,7 @@ def contains_mv():
 def contains_rm():
     """command "rm" is present"""
     output = check50.run("grep -c -w 'rm' typescript").stdout()   
-    if output == "0":
+    if output == "0\n":
         help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
         raise check50.Failure(help)
    
@@ -34,7 +34,7 @@ def contains_rm():
 def contains_rmdir():
     """command "rmdir" is present"""
     output = check50.run("grep -c -w 'rmdir' typescript").stdout()   
-    if output == "0":
+    if output == "0\n":
         help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
         raise check50.Failure(help)
    
@@ -42,7 +42,7 @@ def contains_rmdir():
 def contains_cat():
     """command "cat" is present"""
     output = check50.run("grep -c -w 'cat' typescript").stdout()   
-    if output == "0":
+    if output == "0\n":
         help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
         raise check50.Failure(help)
     
@@ -50,7 +50,7 @@ def contains_cat():
 def contains_more():
     """command "more" is present"""
     output = check50.run("grep -c -w 'more' typescript").stdout()   
-    if output == "0":
+    if output == "0\n":
         help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
         raise check50.Failure(help)
 
@@ -59,7 +59,7 @@ def contains_more():
 def contains_head():
     """command "head" is present"""
     output = check50.run("grep -c -w 'head' typescript").stdout()   
-    if output == "0":
+    if output == "0\n":
         help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
         raise check50.Failure(help)
         
@@ -67,7 +67,7 @@ def contains_head():
 def contains_tail():
     """command "tail" is present"""
     output = check50.run("grep -c -w 'tail' typescript").stdout()   
-    if output == "0":
+    if output == "0\n":
         help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
         raise check50.Failure(help)
 
@@ -77,7 +77,7 @@ def contains_tail():
 def contains_grep():
     """command "grep" is present"""
     output = check50.run("grep -c -w 'grep' typescript").stdout()   
-    if output == "0":
+    if output == "0\n":
         help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
         raise check50.Failure(help)
         
@@ -86,6 +86,6 @@ def contains_grep():
 def contains_wc():
     """command "wc" is present"""
     output = check50.run("grep -c -w 'wc' typescript").stdout()   
-    if output == "0":
+    if output == "0\n":
         help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
         raise check50.Failure(help)
