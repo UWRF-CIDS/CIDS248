047d360951aa5e1eb74d9cd7355be54b670ee859
uwrf-csis <13894601+uwrf-csis@users.noreply.github.com>
2020-12-21 12:53:31 -0600

    Update __init__.py

--- lab1/__init__.py
+++ lab1/__init__.py
@@ -8,32 +8,47 @@ def exists():
 
 @check50.check(exists)
 def contains_ls():
-    """command ls is present"""
-    check50.run("grep -c -w 'ls' typescript").stdout("1")
+    """command "ls" is present"""
+    output = check50.run("grep -c -w 'ls' typescript").stdout()   
+    if output == "0":
+        help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
+        raise check50.Failure(help)
 
 @check50.check(exists)
 def contains_lsa():
-    """command ls -a is present"""
-    check50.run("grep -c -w 'ls -a' typescript").stdout("1")
+    """command "ls -a" is present"""
+    output = check50.run("grep -c -w 'ls -a' typescript").stdout()   
+    if output == "0":
+        help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
+        raise check50.Failure(help)
   
 @check50.check(exists)
 def contains_mkdir():
-    """command mkdir is present"""
-    check50.run("grep -c -w 'mkdir' typescript").stdout("1")
+    """command "mkdir" is present"""
+    output = check50.run("grep -c -w 'mkdir' typescript").stdout()   
+    if output == "0":
+        help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
+        raise check50.Failure(help)
    
 @check50.check(exists)
 def contains_cd():
-    """command cd is present"""
-    check50.run("grep -c -w 'cd' typescript").stdout("1")
+    """command "cd" is present"""
+    output = check50.run("grep -c -w 'cd' typescript").stdout()   
+    if output == "0":
+        help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
+        raise check50.Failure(help)
    
 @check50.check(exists)
 def contains_cdtilde():
-    """command cd ~ is present"""
-    check50.run("grep -c -w 'cd ~' typescript").stdout("1")
+    """command "cd ~" is present"""
+    output = check50.run("grep -c -w 'cd ~' typescript").stdout()   
+    if output == "0":
+        help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
+        raise check50.Failure(help)
     
 @check50.check(exists)
 def contains_cddotdot():
-    """command cd .. is present"""
+    """command "cd .." is present"""
     output = check50.run("grep -c -w 'cd ..' typescript").stdout()   
     if output == "0":
         help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
@@ -43,4 +58,7 @@ def contains_cddotdot():
 @check50.check(exists)
 def contains_pwd():
     """command "pwd" is present"""
-    check50.run("grep -c -w 'pwd' typescript").stdout("1")
+    output = check50.run("grep -c -w 'pwd' typescript").stdout()   
+    if output == "0":
+        help = "Make sure that you have tried all commands in the lab. To start the script command so that it appends to you typescript file, use 'script -a typescript'"
+        raise check50.Failure(help)
