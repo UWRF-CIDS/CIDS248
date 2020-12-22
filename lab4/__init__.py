b8e7be7d0e9f3bf2aa283140607b71b01aae9a7a
Jacob <jacob.hendricks@uwrf.edu>
2020-12-21 21:19:16 -0600

    file IO test

--- lab4/__init__.py
+++ lab4/__init__.py
@@ -15,15 +15,11 @@ def compiles():
     check50.c.compile("redir.c", lreadline=True, lcs50=True)
 
 @check50.check(compiles)
-def emma():
-    check50.run("./redir").exit()
-
-@check50.check(compiles)
-def contains_cat():
+def correctIO():
     """Reads input.txt and outputs first line to tmp.txt"""
     check50.run("./redir")
     with open("input.txt", "w") as f1:
         with open("tmp.txt", "w") as f2:
             if f1.readline() != f2.readline():
-		          help = "no bueno"
+                help = "no bueno"
                 raise check50.Failure(help)
