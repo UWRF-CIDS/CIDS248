014942cb66231af7c615f7e85510f0a2c64c1e3d
Jacob <jacob.hendricks@uwrf.edu>
2021-02-12 17:33:26 -0600

    added lab 8 basic checks

--- lab8/__init__.py
+++ lab8/__init__.py
@@ -11,6 +11,30 @@ def table_exists():
     """table.c exists"""
     check50.exists("table.c")
     check50.include("table_soln")
+    
+@check50.check()
+def strings1_exists():
+    """strings1.c exists"""
+    check50.exists("strings1.c")
+    check50.include("strings1_soln")
+    
+@check50.check()
+def strings2_exists():
+    """strings2.c exists"""
+    check50.exists("strings2.c")
+    check50.include("strings2_soln")
+
+@check50.check()
+def strings3_exists():
+    """strings3.c exists"""
+    check50.exists("strings3.c")
+    check50.include("strings3_soln")
+    
+@check50.check()
+def input_exists():
+    """input.c exists"""
+    check50.exists("input.c")
+    check50.include("input_soln")
 
 @check50.check(table_exists)
 def table_compiles():
