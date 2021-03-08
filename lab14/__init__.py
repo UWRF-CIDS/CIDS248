cf11c3afa8d47d5593531f87cbfda480d4732d43
Jacob <jacob.hendricks@uwrf.edu>
2021-03-08 10:49:20 -0600

    added lab14 checks

--- lab14/__init__.py
+++ lab14/__init__.py
@@ -17,8 +17,8 @@ def leaky_compiles():
 @check50.check(leaky_compiles)
 def leaky_check():
     """leaky.c contains no memory leaks"""
-    output = check50.run("./leaky".stdout()
-    raise check50.Failure(output)
+    output = check50.run(valgrind "./leaky").stdout()
+    
     if "All heap blocks were freed -- no leaks are possible" not in str(output):
         raise check50.Failure("According to valgrind, the program still contains leaks.")
 
