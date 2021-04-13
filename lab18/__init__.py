1073ab411d1c89f2cac683b2fc40377f954f78c5
Jacob <jacob.hendricks@uwrf.edu>
2021-04-13 14:13:42 -0500

    added lab 18 checks

---
+++ lab18/__init__.py
@@ -0,0 +1,26 @@
+import check50
+import check50.c
+import random
+import os.path
+
+
+@check50.check()
+def io_example_exists():
+    """simple_fork exist"""
+    check50.exists("io_example.c")
+
+@check50.check(io_example_exists)
+def io_example_compiles():
+    """io_example compiles"""
+    check50.c.compile("io_example.c",cc='gcc', lcs50=False)
+
+@check50.check()
+def simple_wait_exists():
+    """dup_example exist"""
+    check50.exists("dup_example.c")
+
+@check50.check(dup_example_exists)
+def dup_example_compiles():
+    """dup_example compiles"""
+    check50.c.compile("dup_example.c",cc='gcc', lcs50=False)
+    
